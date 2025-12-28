026767b0f5296ef62e3962983f3381af5b3be736
Noah Alasin <njalasin18@gmail.com>
2025-12-28 03:27:26 -0500

    Folder cleanup
    move two scripts from scenes folder to scripts folder

---
+++ Project-Flow-State/Scripts/item_basic_sniper.gd
@@ -0,0 +1,16 @@
+extends RigidBody3D
+
+@onready var label: Label3D = $Label3D
+var dropped = false
+
+func _process(delta):
+	if dropped == true:
+		apply_impulse(Vector3(3, 0, 0))
+		dropped = false
+func interact():
+	print("Picked up Basic Sniper")
+	queue_free()
+func show_label():
+	label.visible = true
+func hide_label():
+	label.visible = false
